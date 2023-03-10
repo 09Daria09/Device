0460554b4bbf3a952c81e1f26e29a8272f6db78f
Daria <dashakukuruza86@gmail.com>
2023-03-10 10:45:35 +0200

    Serializator

--- Device/Program.cs
+++ Device/Program.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static Device.Program;
 
 namespace Device
 {
-    internal class Program
+    public class Program
     {
+        [Serializable]
+        [DataContract]
         public class DEvice
         {
+           [DataMember]
             public string Name { get; set; }
+            [DataMember]
             public string Manufacturer { get; set; }
+            [DataMember]
             public double Price { get; set; }
 
             public DEvice()
@@ -45,41 +56,84 @@ namespace Device
         static void Main(string[] args)
         {
             List<DEvice> Devices = new List<DEvice>{
-    new DEvice("Laptop", "Dell", 1500.00),
-    new DEvice("Smartphone", "Samsung", 800.00),
-    new DEvice("Tablet", "Apple", 1000.00),
-    new DEvice("Desktop", "HP", 1200.00),
-    new DEvice("Smartwatch", "Fitbit", 200.00),
-    new DEvice("Smart speaker", "Amazon", 100.00)};
+                                 new DEvice("Laptop", "Dell", 1500.00),
+                               new DEvice("Smartphone", "Samsung", 800.00),
+                               new DEvice("Tablet", "Apple", 1000.00),
+                               new DEvice("Desktop", "HP", 1200.00),
+                               new DEvice("Smartwatch", "Fitbit", 200.00),
+                               new DEvice("Smart speaker", "Amazon", 100.00)};
 
             List<DEvice> Devices1 = new List<DEvice>{
-    new DEvice("Smart TV", "Samsung", 1200.00),
-    new DEvice("Gaming laptop", "Asus", 2000.00),
-    new DEvice("Gaming console", "Sony", 500.00),
-    new DEvice("E-book reader", "Kobo", 150.00),
-    new DEvice("Virtual assistant", "Google", 50.00),
-    new DEvice("Wireless earbuds", "Apple", 250.00)};
+                                new DEvice("Smart TV", "Samsung", 1200.00),
+                               new DEvice("Gaming laptop", "Asus", 2000.00),
+                               new DEvice("Gaming console", "Sony", 500.00),
+                               new DEvice("E-book reader", "Kobo", 150.00),
+                               new DEvice("Virtual assistant", "Google", 50.00),
+                               new DEvice("Wireless earbuds", "Apple", 250.00)};
 
-            Console.WriteLine("\t*** Разница ***");
-            var difference = Devices.Except(Devices1, new Sort());
-            foreach (var device in difference)
+            char answer;
+            FileStream stream = null;
+            BinaryFormatter formatter = null;
+            XmlSerializer serializer = null;
+            string s = String.Empty;
+            
+
+            stream = new FileStream("list.xml", FileMode.Create);
+            serializer = new XmlSerializer(typeof(List<DEvice>));
+            serializer.Serialize(stream, Devices);
+            stream.Close();
+            Console.WriteLine("Сериализация успешно выполнена!");
+
+            stream = new FileStream("list.xml", FileMode.Open);
+            serializer = new XmlSerializer(typeof(List<DEvice>));
+            Devices = (List<DEvice>)serializer.Deserialize(stream);
+            foreach (DEvice j in Devices)
             {
-                Console.WriteLine($"{device.Name} ({device.Manufacturer}): ${device.Price}");
+                Console.WriteLine($"{j.Name} ({j.Manufacturer}): ${j.Price}");
             }
-            Console.WriteLine();
-            Console.WriteLine("\t*** Обьединение ***");
-            var union = Devices.Union(Devices1, new Sort());
-            foreach (var device in union)
+            //Console.WriteLine(s);
+            stream.Close();
+
+            using (stream = new FileStream("list.json", FileMode.Create))
             {
-                Console.WriteLine($"{device.Name} ({device.Manufacturer}): ${device.Price}");
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<DEvice>));
+                jsonFormatter.WriteObject(stream, Devices1);
+                Console.WriteLine("Сериализация выполнена успешно!");
             }
-            Console.WriteLine();
-            Console.WriteLine("\t*** Пересечение ***");
-            var intersection = Devices.Intersect(Devices1, new Sort());
-            foreach (var device in intersection)
+
+            // Читаем сериализованный список из файла и выводим информацию о каждом устройстве
+            using (stream = new FileStream("list.json", FileMode.Open))
             {
-                Console.WriteLine($"{device.Name} ({device.Manufacturer}): ${device.Price}");
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<DEvice>));
+                List<DEvice> deserializedDevices = (List<DEvice>)jsonFormatter.ReadObject(stream);
+                foreach (DEvice device in deserializedDevices)
+                {
+                    Console.WriteLine($"{device.Name} ({device.Manufacturer}): ${device.Price}");
+                }
+                Console.WriteLine("Десериализация выполнена успешно!");
             }
+
+
+            //Console.WriteLine("\t*** Разница ***");
+            //var difference = Devices.Except(Devices1, new Sort());
+            //foreach (var device in difference)
+            //{
+            //    Console.WriteLine($"{device.Name} ({device.Manufacturer}): ${device.Price}");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("\t*** Обьединение ***");
+            //var union = Devices.Union(Devices1, new Sort());
+            //foreach (var device in union)
+            //{
+            //    Console.WriteLine($"{device.Name} ({device.Manufacturer}): ${device.Price}");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("\t*** Пересечение ***");
+            //var intersection = Devices.Intersect(Devices1, new Sort());
+            //foreach (var device in intersection)
+            //{
+            //    Console.WriteLine($"{device.Name} ({device.Manufacturer}): ${device.Price}");
+            //}
         }
     }
 }
